0fe7220bb2f2c386b0228d57e9d748ed32a1423d
Ehmkaay <mk.agbola@gmail.com>
2024-04-06 03:17:33 -0700

    client confit with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+# ssh_config_manifest.pp
+
+# Ensure the SSH client configuration directory exists
+file { '/home/ubuntu/.ssh':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0700',
+  require => File['/home/ubuntu'],
+}
+
+# Configure SSH client to use the private key and refuse password authentication
+file { '/home/ubuntu/.ssh/config':
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "Host 18.235.233.220\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+  require => File['/home/ubuntu/.ssh'],
+}
